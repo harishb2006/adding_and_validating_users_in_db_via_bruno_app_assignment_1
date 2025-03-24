f84e4f0ff365ec8a425eff380037b6126fa78d13
harishb2006 <imharishba@gmail.com>
2025-03-24 12:25:36 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const { resolve } = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose'); 
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.DB_URL, )
+.then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  try {
+ 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 app.use(express.static('static'));
 
